d7fc076309f1a93843158880a82c23ad3d3d65d9
Ishfaq Ahmed <ishfaq.ahmed@arbisoft.com>
2019-11-28 15:40:10 +0500

    admin page counter problem resolved

--- DSRStore/project/static/js/counter.js
+++ DSRStore/project/static/js/counter.js
@@ -1,14 +1,14 @@
-function increaseValue() {
-  var value = parseInt(document.getElementById('number').value, 10);
+function increaseValue(valueid) {
+  var value = parseInt(document.getElementById(valueid).value, 10);
   value = isNaN(value) ? 0 : value;
   value++;
-  document.getElementById('number').value = value;
+  document.getElementById(valueid).value = value;
 }
 
-function decreaseValue() {
-  var value = parseInt(document.getElementById('number').value, 10);
+function decreaseValue(valueid) {
+  var value = parseInt(document.getElementById(valueid).value, 10);
   value = isNaN(value) ? 0 : value;
   value < 1 ? value = 1 : '';
   value--;
-  document.getElementById('number').value = value;
+  document.getElementById(valueid).value = value;
 }
